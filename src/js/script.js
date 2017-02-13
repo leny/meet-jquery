6161ff387d6d6669abb2b5ccf170ebf657e9fd8b
leny <info@flatland.be>
2017-02-13 16:38:47 +0100

    Exercise two

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,25 @@
  * started at 13/02/2017
  */
 
+const fHandleTab = function( oEvent ) {
+    let $this = $( this );
+
+    oEvent.preventDefault();
+    if ( $this.parent().hasClass( "active" ) ) {
+        return;
+    }
+    $( "ul.nav.nav-tabs .active" ).removeClass( "active" );
+    $this.parent().addClass( "active" );
+    $( ".tab-pane.active" ).removeClass( "active" );
+    $( `#${ $this.data( "tab-target" ) }` ).addClass( "active" );
+};
+
 $( function() {
-    
+
     // 1. a with rel=external
     $( 'a[rel*="external"]' ).attr( "target", "_new" );
+
+    // 2. tabs
+    $( "ul.nav.nav-tabs a" ).on( "click", fHandleTab );
 
 } );
