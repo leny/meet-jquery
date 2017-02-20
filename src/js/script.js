7e490c78a7b68aba8c10e99c2291338f6d39675c
leny <info@flatland.be>
2017-02-20 10:20:58 +0100

    Exercise four - part one

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,9 @@
  * started at 13/02/2017
  */
 
-let $trombinoFigures;
+const rEmailValidation = /([\w-\.]+)@((?:[\w]+\.)+)([a-z]{2,})/i;
+
+let $trombinoFigures, $commentForm, $emailInput, $nameInput, $commentTextarea;
 
 const fHandleTab = function( oEvent ) {
     let $this = $( this );
@@ -31,6 +33,45 @@ const fHandleTrombino = function() {
     } );
 };
 
+const fHandleFormValidation = function( oEvent ) {
+    let bHasErrors = false,
+        sEmail, sName, sComment;
+
+    // 1. check email
+    sEmail = ( $emailInput.val() || "" ).trim();
+    if ( !rEmailValidation.test( sEmail ) ) {
+        console.error( "Email isn't valid!" );
+        bHasErrors = true;
+    } else {
+        console.info( "Email is valid!" );
+    }
+
+    // 2. check name
+    sName = ( $nameInput.val() || "" ).trim();
+    if ( sName.length < 4 ) {
+        console.error( "Name isn't valid!" );
+        bHasErrors = true;
+    } else {
+        console.info( "Name is valid!" );
+    }
+
+    // 3. check comment
+    sComment = ( $commentTextarea.val() || "" ).trim();
+    if ( sComment.length < 10 || sComment.length > 140 ) {
+        console.error( "Comment isn't valid!" );
+        bHasErrors = true;
+    } else {
+        console.info( "Comment is valid!" );
+    }
+
+    if ( bHasErrors ) {
+        window.alert( "Veuillez remplir tous les champs de formulaire!" );
+        return false;
+    }
+
+    return true;
+};
+
 $( function() {
 
     // 1. a with rel=external
@@ -43,5 +84,12 @@ $( function() {
     $trombinoFigures = $( "#trombino figure" );
     $trombinoFigures.hide().first().show();
     setInterval( fHandleTrombino, 1000 );
+
+    // 4. handle form validation
+    $commentForm = $( "form" );
+    $emailInput = $( "#inputEmail" );
+    $nameInput = $( "#inputName" );
+    $commentTextarea = $( "#inputComment" );
+    $commentForm.on( "submit", fHandleFormValidation );
 
 } );
